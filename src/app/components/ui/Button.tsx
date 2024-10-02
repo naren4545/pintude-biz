c03eb61cad38f6338f7afcea1897b83bbe02dcec
naren4545 <narenchavan26@gmail.com>
2024-10-02 15:17:55 +0530

    Your commit message here

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -1,6 +1,12 @@
+import { ReactNode } from "react";
 
+interface ButtonProps {
+  children: ReactNode;
+  href: string;
+  className?: string; // Optional prop
+}
 
-export default function Button({children,href , className}) {
+export default function Button({ children, href, className = "" }: ButtonProps) {
   return (
    <a href={href} className={"bg-[#F6830E] py-4 px-8 text-center min-w-[160px] text-white " +className}>{children}</a>
   )

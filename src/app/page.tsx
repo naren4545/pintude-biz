9a4f6d050ad7c471f6179218202988732cb63573
naren4545 <narenchavan26@gmail.com>
2024-10-22 14:01:31 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 import Image from "next/image";
-import Header from "./components/home/Header";
-import SectionOne from "./components/home/sectionOne";
-import MapUI from "./components/home/MapUI";
-import GallerySection from "./components/home/GalerySec";
-import BuisnessCard from "./components/home/BuisnessCard";
-import ResponsiveCard from "./components/ui/CardCustom";
-import CardCustom2 from "./components/ui/Cardcustom2";
-import SectionCard from "./components/home/SectionCard";
 
-import ResponsiveSlider from "./components/home/ResponsiveSlider";
-import Footer from "./components/home/Footer";
 import HomePG from "./components/home/Home";
 export default function Home() {
   return (
